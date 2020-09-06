0a111e01bf4d3f851a3c34936fbb1bd4592fb494
joeatron <63060723+joeatron@users.noreply.github.com>
2020-09-06 23:44:06 +0100

    Add files via upload

---
+++ Robot arena 2 bot test/Sonic (lines).py
@@ -0,0 +1,106 @@
+#Cube 1
+#00:28 01/09/2020 
+#----IMPORT AREA----
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+#----setup zone----
+vertices = (        #-Note (X,Y,Z)
+    (0, 0.45, 0.5),        #0
+    (-0.1, 0.45, 0.5),     #1
+    (-0.5, 0.45, 0.2),   #2
+    (-0.5, 0, 0.2),     #3
+    (-0.1, 0, 0.55),       #4
+    (1, 0, 0.5),          #5
+    (0, 0.45, -0.5),       #6
+    (-0.1, 0.45, -0.5),    #7
+    (-0.5, 0.45, -0.2),  #8
+    (-0.5, 0, -0.2),    #9
+    (-0.1, 0, -0.55),       #10
+    (1, 0, -0.5),         #11
+    )
+
+edges = (
+    (0,1),
+    (0,5),
+    (0,6),
+    (1,4),
+    (1,2),
+    (2,8),
+    (2,3),
+    (3,4),
+    (3,9),
+    (4,5),
+    (5,11),
+    (6,7),
+    (6,11),
+    (7,8),
+    (7,10),
+    (8,9),
+    (9,10),
+    (10,11),
+    )
+
+#----render code----
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+#----main----
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1 , 50.0)
+
+    glTranslate(0,0, -5)   #-Note (x,y,z)
+    glRotatef(0, 0, 0, 0)   #-Note (angle , x , y , z)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit
+
+        keys = pygame.key.get_pressed()
+        #Arrow_keys
+        if keys[pygame.K_a]:
+            glTranslate(-0.1,0,0)
+        if keys[pygame.K_d]:
+            glTranslate(0.1,0,0)
+        if keys[pygame.K_w]:
+            glTranslate(0,0.1,0)
+        if keys[pygame.K_s]:
+            glTranslate(0,-0.1,0)
+        #buttons
+        if keys[pygame.K_q]:
+            glTranslate(0,0,0.1)
+        if keys[pygame.K_e]:
+            glTranslate(0,0,-0.1)
+        #numpad
+        if keys[pygame.K_LEFT]:
+            glRotatef(1, 0, 1, 0)
+        if keys[pygame.K_RIGHT]:
+            glRotatef(1, 0, -1, 0)
+        if keys[pygame.K_UP]:    
+            glRotatef(1, 1, 0, 0)
+        if keys[pygame.K_DOWN]:
+            glRotatef(1, -1, 0, 0)
+        if keys[pygame.K_m]:    
+            glRotatef(1, 0, 0, -1)
+        if keys[pygame.K_n]:
+            glRotatef(1, 0, 0, 1)
+           
+
+
+                    
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(1)
+main()
---
+++ cube/CUBE.py
@@ -0,0 +1,96 @@
+#Cube 1
+#00:28 01/09/2020 
+#----IMPORT AREA----
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+#----setup zone----
+#Xmove = 0
+#Ymove = 0
+#Zmove = 0
+#----
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1),
+    )
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7),
+    )
+
+#----render code----
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+#----main----
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1 , 50.0)
+
+    glTranslate(0,0, -10)   #-Note (x,y,z)
+    glRotatef(0, 0, 0, 0)   #-Note (angle , x , y , z)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit
+
+        keys = pygame.key.get_pressed()
+        #Arrow_keys
+        if keys[pygame.K_a]:
+            glTranslate(-0.1,0,0)
+        if keys[pygame.K_d]:
+            glTranslate(0.1,0,0)
+        if keys[pygame.K_w]:
+            glTranslate(0,0.1,0)
+        if keys[pygame.K_s]:
+            glTranslate(0,-0.1,0)
+        #buttons
+        if keys[pygame.K_q]:
+            glTranslate(0,0,0.1)
+        if keys[pygame.K_e]:
+            glTranslate(0,0,-0.1)
+        #numpad
+        if keys[pygame.K_LEFT]:
+            glRotatef(1, 0, 1, 0)
+        if keys[pygame.K_RIGHT]:
+            glRotatef(1, 0, -1, 0)
+        if keys[pygame.K_UP]:    
+            glRotatef(1, 1, 0, 0)
+        if keys[pygame.K_DOWN]:
+            glRotatef(1, -1, 0, 0)
+           
+
+
+                    
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(1)
+main()
